fe5708a1e2e563781b205ddd4ce72f50e12e1757
sameer4712 <sameerptmcgmail.com>
2025-07-14 11:31:01 +0530

    app

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import routes from './router/routes.js'
+import productRoutes from './router/productsRoutes.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.set('view engine','ejs')
 
 const uri = ("mongodb://localhost:27017/PostMan")
 mongoose.connect(uri).then(() => {
@@ -13,7 +16,7 @@ mongoose.connect(uri).then(() => {
 })
 
 app.use(routes)
-
+app.use(productRoutes)
 app.listen(3000,()=>{
     console.log("Server started");
     
