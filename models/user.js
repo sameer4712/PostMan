584b1843f137e0b704a31214f9734e36489b5bf9
sameer4712 <sameerptmcgmail.com>
2025-07-14 11:31:17 +0530

    user

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     name:String,
     age:Number,
-    place:String
+    place:String,
+    email:String,
+    password:String
 })
 
 const variable = mongoose.model('people',userSchema)
 
-export default variable
+export default  variable
