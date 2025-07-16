76130c912432269873a9f70b73908d07a3af0a57
sameer4712 <sameerptmcgmail.com>
2025-07-16 15:59:01 +0530

    SQDWWD

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 import express from 'express'
+import session from 'express-session'
 import mongoose from 'mongoose'
 import routes from './router/routes.js'
 import productRoutes from './router/productsRoutes.js'
+// import variable from './models/user.js'
 
 const app = express()
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 
-app.set('view engine','ejs')
+app.use(session(
+    {
+        secret: "my secret key",
+        saveUninitialized: true,
+        resave: false
+    }
+)
+
+
+
+)
+
+app.set('view engine', 'ejs')
 
 const uri = ("mongodb://localhost:27017/PostMan")
 mongoose.connect(uri).then(() => {
@@ -16,9 +31,11 @@ mongoose.connect(uri).then(() => {
 })
 
 app.use(routes)
+
 app.use(productRoutes)
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
     console.log("Server started");
-    
+
 })
 
